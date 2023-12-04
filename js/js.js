6dea387932570464b7ab1a515d83d65390b6a090
boroeurnprach <prach.boroeurn@gmail.com>
2023-12-04 18:05:11 +0700

    first commit

---
+++ js/js.js
@@ -0,0 +1,21 @@
+/* JavaScript ជាភាសាសម្រាប់ធ្វើឲ្យព័ត៌មានលើផេកឬរចនាសម្ព័ន្ធវេបសាយផ្លាស់ប្ដូរពេលវាកំពុងដំណើរកាដោយប្រើធនធាន
+ (resource) របស់ម៉ាស៊ីនអ្នទស្សនា។ */
+
+//  dynamic webpage or dynamic website
+//  -it can chnag during working.
+//      1. cilent side or front-end
+//          use client's resource
+//      2. server side or back end
+//          use server's resource (hardware, sotware, files, services)
+
+//      How to wrie
+//          inline script : write in script in bone
+//          <...on_event = 'javascript code;'></>
+//          internal script write the script in webpage.
+//          <script>
+//          javascript code;
+
+
+//  */
+
+window.alert("extenal script");
